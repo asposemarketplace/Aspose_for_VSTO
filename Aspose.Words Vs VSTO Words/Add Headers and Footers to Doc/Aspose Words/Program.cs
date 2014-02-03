12017b4b9d9a2741efd4efb15d021405c9456053
zeeshan.shafqat@aspose.com <zeeshan.shafqat@aspose.com>
2014-02-03 22:54:26 +0500

    New Projects Added

--- Aspose.Words Vs VSTO Words/Add Headers and Footers to Doc/Aspose Words/Program.cs
+++ Aspose.Words Vs VSTO Words/Add Headers and Footers to Doc/Aspose Words/Program.cs
@@ -1,32 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Aspose.Words;
+﻿using Aspose.Words;
 namespace Aspose_Words
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string mypath ="";
+            string mypath = "";
             Document doc = new Document();
             DocumentBuilder builder = new DocumentBuilder(doc);
-            Section currentSection = builder.CurrentSection;
-            PageSetup pageSetup = currentSection.PageSetup;
-            // --- Create header for the first page. ---
-            pageSetup.HeaderDistance = 20;
-            builder.MoveToHeaderFooter(HeaderFooterType.HeaderFirst);
-            builder.ParagraphFormat.Alignment = ParagraphAlignment.Center;
-              
-            // Set font properties for header text.
-            builder.Font.Name = "Arial";
-            builder.Font.Bold = true;
-            builder.Font.Size = 14;
-            // Specify header title for the first page.
-            builder.Write("Header - Title Page.");
+            // Create the headers.
+            builder.MoveToHeaderFooter(HeaderFooterType.HeaderPrimary);
+            builder.Write("Header Text goes here...");
+            //add footer having current date
+            builder.MoveToHeaderFooter(HeaderFooterType.FooterPrimary);
+            builder.InsertField("Date", "");
 
+            doc.UpdateFields();
             doc.Save(mypath + "Insert Headers and Footers.doc");
         }
     }

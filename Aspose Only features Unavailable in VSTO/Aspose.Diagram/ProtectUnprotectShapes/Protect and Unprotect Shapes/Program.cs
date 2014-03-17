4c60096676506f71f5ca41d8768f0bf25e4729c7
zeeshan.shafqat@aspose.com <zeeshan.shafqat@aspose.com>
2014-03-17 14:10:21 +0500

    New Projects Added

---
+++ Aspose Only features Unavailable in VSTO/Aspose.Diagram/ProtectUnprotectShapes/Protect and Unprotect Shapes/Program.cs
@@ -0,0 +1,45 @@
+﻿using Aspose.Diagram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protect_and_Unprotect_Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string MyDir = @"Files\";
+            //Load diagram
+            Diagram diagram = new Diagram(MyDir+"ProtectShape.vsd");
+
+            Page page0 = diagram.Pages[0];
+
+            Shape shape = page0.Shapes[0];
+            shape.Protection.LockAspect.Value = BOOL.True;
+            shape.Protection.LockBegin.Value = BOOL.True;
+            shape.Protection.LockCalcWH.Value = BOOL.True;
+            shape.Protection.LockCrop.Value = BOOL.True;
+            shape.Protection.LockCustProp.Value = BOOL.True;
+            shape.Protection.LockDelete.Value = BOOL.True;
+            shape.Protection.LockEnd.Value = BOOL.True;
+            shape.Protection.LockFormat.Value = BOOL.True;
+            shape.Protection.LockFromGroupFormat.Value = BOOL.True;
+            shape.Protection.LockGroup.Value = BOOL.True;
+            shape.Protection.LockHeight.Value = BOOL.True;
+            shape.Protection.LockMoveX.Value = BOOL.True;
+            shape.Protection.LockMoveY.Value = BOOL.True;
+            shape.Protection.LockRotate.Value = BOOL.True;
+            shape.Protection.LockSelect.Value = BOOL.True;
+            shape.Protection.LockTextEdit.Value = BOOL.True;
+            shape.Protection.LockThemeColors.Value = BOOL.True;
+            shape.Protection.LockThemeEffects.Value = BOOL.True;
+            shape.Protection.LockVtxEdit.Value = BOOL.True;
+            shape.Protection.LockWidth.Value = BOOL.True;
+            diagram.Save(MyDir+"ProtectedShapesFile.vdx", SaveFileFormat.VDX);
+        }
+    }
+}
